7ffa84d82cabedead8c24bea9c664fbaf6d1741a
Johnathon Sykes <hireme@mrjsykes.com>
2017-06-26 11:54:36 -0700

    added seed data, user controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'user/index'
+
+  get 'user/show'
+
+  get 'user/new'
+
+  get 'user/create'
+
+  get 'user/edit'
+
+  get 'user/update'
+
+  get 'user/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
