1575a086eed91113473e1549c3d458b51b436d8d
Johnathon Sykes <hireme@mrjsykes.com>
2017-06-28 14:37:57 -0700

    Added avatar_url to User table and config for image upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,21 @@ class User < ApplicationRecord
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness:true
 
+  has_attached_file :avatar_url,
+  styles: {thumb: "200x200#"},
+  storage: :s3,
+  url: ":s3_domain_url",
+  path: '/:class/:attachment/:id_partition/:style/:filename',
+  s3_region: ENV["S3_REGION"],
+  s3_credentials: Proc.new { |a| a.instance.s3_credentials}
+
+  def s3_credentials
+    bucket: ENV["S3_BUCKET_NAME"]
+    access_key_id: ENV["S3_ACCESS_KEY_ID"]
+    secret_access_key: ENV["S3_SECRET_ACCESS_KEY"]
+
+    validates_attachment_content_type :avatar_url, content_type: /\Aimage\/.*\z/
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170628193219) do
+ActiveRecord::Schema.define(version: 20170628212513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20170628193219) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "user_name", limit: 20
+    t.string "avatar_url"
   end
 
   add_foreign_key "movies", "users"
