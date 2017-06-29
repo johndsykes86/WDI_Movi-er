6b5e287a99e41534f16693a5b37b8d2f07168b25
Johnathon Sykes <hireme@mrjsykes.com>
2017-06-29 10:00:15 -0700

    Added poster attribute to Movie db

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,6 @@
 class Movie < ApplicationRecord
   belongs_to :user
+
+  has_attached_file :poster, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :poster, content_type: /\Aimage\/.*\z/
 end
