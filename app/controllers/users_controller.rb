d5cd48cf8233f75999fb2ae234a36c9267d2f889
Johnathon Sykes <hireme@mrjsykes.com>
2017-06-29 14:37:17 -0700

    Added user_name field to sign-up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,6 @@ before_action :authorize, only: [:show]
 
   private
   def user_params
-   params.require(:user).permit( :email, :first_name, :last_name, :password, :password_confirmation)
+   params.require(:user).permit( :email, :first_name, :last_name, :user_name, :password, :password_confirmation)
   end
 end
